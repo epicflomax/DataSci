2e9a961366f2a4dd706e8e8e4e1bd0a0b23b2a04
epicflomax <mmargreit@gmail.com>
2025-05-08 16:31:50 +0200

    Update Rename.py

--- Rename.py
+++ Rename.py
@@ -10,5 +10,5 @@ df = pd.read_csv("kzp-2008-2020-timeseries.csv",encoding="latin1")
 df_2011=df[df["JAHR"]==2011]
 
 y = df_2011["FiErg"]
-X = df_2011[["FiErg","KT","Inst", "Adr"  "Ort", "Typ", "RWStatus", "Akt", "SL", "WB", "AnzStand","SA","PtageStatT","AustStatT","NeugStatT","Ops","Gebs","CMIb","CMIn",
+X = df_2011[["FiErg","KT","Inst", "Adr",  "Ort", "Typ", "RWStatus", "Akt", "SL", "WB", "AnzStand","SA","PtageStatT","AustStatT","NeugStatT","Ops","Gebs","CMIb","CMIn",
             "pPatWAU","pPatWAK", "pPatLKP","pPatHOK","PersA","PersP","PersMT","PersT","PersAFall","PersPFall","PersMTFall","PersTFall","AnzBelA","AnzBelP"]]
