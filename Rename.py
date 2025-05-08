a2118e53f90dd59fae3e6c5cff0399c84bc906d0
nata <natalie.haldner@gmail.com>
2025-05-08 17:31:52 +0200

    num, cat features separation

--- Rename.py
+++ Rename.py
@@ -3,22 +3,34 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import roc_curve, confusion_matrix, auc
 import matplotlib.pyplot as plt
-#meow
+
 
 df = pd.read_csv("kzp-2008-2020-timeseries.csv",encoding="latin1")
 
-df_2011=df[df["JAHR"]==2011]
+data =df[df["JAHR"]==2011].copy()
 
-y = df_2011["FiErg"]
-X = df_2011[["KT","Inst", "Adr",  "Ort", "Typ", "RWStatus", "Akt", "SL", "WB", "AnzStand","SA","PtageStatT","AustStatT","NeugStatT","Ops","Gebs","CMIb","CMIn",
+label = data["FiErg"]
+features = data[["KT","Inst", "Adr",  "Ort", "Typ", "RWStatus", "Akt", "SL", "WB", "AnzStand","SA","PtageStatT","AustStatT","NeugStatT","Ops","Gebs","CMIb","CMIn",
             "pPatWAU","pPatWAK", "pPatLKP","pPatHOK","PersA","PersP","PersMT","PersT","PersAFall","PersPFall","PersMTFall","PersTFall","AnzBelA","AnzBelP (nur ab KZP2010)"]]
 
-print(X.isna().sum())
-missing_percentage = X.isnull().mean() * 100
+print(features.isna().sum())
+missing_percentage = features.isnull().mean() * 100
 
 print(missing_percentage[missing_percentage > 50].sort_values(ascending=False))
 
+
+num_features = features.select_dtypes("number")
+cat_features = features.select_dtypes(exclude=["number"])
+
+#Percentage of missing values per categorical column
+missing_percentage_cat = (cat_features.isnull().sum() / len(cat_features)) * 100
+print("Missing percentage Categorical columns\n", missing_percentage_cat)
+
+#Percentage of missing values per numeric column
+missing_percentage_num = (num_features.isnull().sum() / len(num_features)) * 100
+print("Missing percentage numeric columns\n", missing_percentage_num)
+
 fill_in = 'NA'
-df['SA'] = df['SA'].fillna(fill_in)
-df['SL'] = df['SL'].fillna(fill_in)
-df['WB'] = df['WB'].fillna(fill_in)
+data['SA'] = data['SA'].fillna(fill_in)
+data['SL'] = data['SL'].fillna(fill_in)
+data['WB'] = data['WB'].fillna(fill_in)
