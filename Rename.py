739c83b02788c0d50eecbdeb7ad010db2d473a59
epicflomax <mmargreit@gmail.com>
2025-05-08 17:06:48 +0200

    Update Rename.py

--- Rename.py
+++ Rename.py
@@ -20,3 +20,4 @@ print(missing_percentage[missing_percentage > 50].sort_values(ascending=False))
 
 df['SA'] = df['your_column_name'].fillna(None)
 df['SL'] = df['your_column_name'].fillna(None)
+df['WB'] = df['your_column_name'].fillna(None)
