0736d988410eaf5dc2aea39708bc7603177adb79
epicflomax <mmargreit@gmail.com>
2025-05-08 16:57:59 +0200

    Update Rename.py

--- Rename.py
+++ Rename.py
@@ -12,3 +12,8 @@ df_2011=df[df["JAHR"]==2011]
 y = df_2011["FiErg"]
 X = df_2011[["KT","Inst", "Adr",  "Ort", "Typ", "RWStatus", "Akt", "SL", "WB", "AnzStand","SA","PtageStatT","AustStatT","NeugStatT","Ops","Gebs","CMIb","CMIn",
             "pPatWAU","pPatWAK", "pPatLKP","pPatHOK","PersA","PersP","PersMT","PersT","PersAFall","PersPFall","PersMTFall","PersTFall","AnzBelA","AnzBelP (nur ab KZP2010)"]]
+
+print(X.isna().sum())
+missing_percentage = X.isnull().mean() * 100
+
+print(missing_percentage[missing_percentage > 50].sort_values(ascending=False))
