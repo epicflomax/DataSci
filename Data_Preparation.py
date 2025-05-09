895c51d511a306c92b00f63198293b07baf3cf2e
Nata <113512445+Erysikthon@users.noreply.github.com>
2025-05-09 09:56:41 +0200

    Adjusted file

--- Data_Preparation.py
+++ Data_Preparation.py
@@ -10,24 +10,28 @@ df = pd.read_csv("kzp-2008-2020-timeseries.csv",encoding="latin1")
 data =df[df["JAHR"]==2011].copy()
 
 label = data["FiErg"]
-features = data[["KT","Inst", "Adr",  "Ort", "Typ", "RWStatus", "Akt", "SL", "WB", "AnzStand","SA","PtageStatT","AustStatT","NeugStatT","Ops","Gebs","CMIb","CMIn",
-            "pPatWAU","pPatWAK", "pPatLKP","pPatHOK","PersA","PersP","PersMT","PersT","PersAFall","PersPFall","PersMTFall","PersTFall","AnzBelA","AnzBelP (nur ab KZP2010)"]]
+features = ["KT","Inst", "Adr",  "Ort", "Typ", "RWStatus", "Akt", "SL", "WB", "AnzStand","SA","PtageStatT","AustStatT","NeugStatT","Ops","Gebs","CMIb","CMIn",
+            "pPatWAU","pPatWAK", "pPatLKP","pPatHOK","PersA","PersP","PersMT","PersT","PersAFall","PersPFall","PersMTFall","PersTFall","AnzBelA","AnzBelP (nur ab KZP2010)"]
 
-print(features.isna().sum())
-missing_percentage = features.isnull().mean() * 100
+#print(data[features].isna().sum())
+#missing_percentage = data[features].isnull().mean() * 100
 
-print(missing_percentage[missing_percentage > 50].sort_values(ascending=False))
+#print(missing_percentage[missing_percentage > 50].sort_values(ascending=False))
 
+#Dropping Adr
+data = data.drop(columns=["Adr"])
+features.remove("Adr")
 
-num_features = features.select_dtypes("number")
-cat_features = features.select_dtypes(exclude=["number"])
+num_features = data[features].select_dtypes("number").columns
+cat_features = data[features].select_dtypes(exclude=["number"]).columns
+
 
 #Percentage of missing values per categorical column
-missing_percentage_cat = (cat_features.isnull().sum() / len(cat_features)) * 100
+missing_percentage_cat = (data[cat_features].isnull().sum() / len(data[cat_features])) * 100
 print("Missing percentage Categorical columns\n", missing_percentage_cat)
 
 #Percentage of missing values per numeric column
-missing_percentage_num = (num_features.isnull().sum() / len(num_features)) * 100
+missing_percentage_num = (data[num_features].isnull().sum() / len(data[num_features])) * 100
 print("Missing percentage numeric columns\n", missing_percentage_num)
 
 #Filling Categorical Columns
@@ -37,5 +41,26 @@ data['SL'] = data['SL'].fillna(fill_in)
 data['WB'] = data['WB'].fillna(fill_in)
 
 #Filling Numeric Columns
-for i in num_features.columns:
+for i in data[num_features]:
     data[i] = data[i].fillna(data[i].mean())
+
+
+#Transforming object col into category
+print(data[cat_features].dtypes)
+data[cat_features] = data[cat_features].astype('category')
+print(data[cat_features].dtypes)
+
+print(data[features])
+
+#check
+
+#Percentage of missing values per categorical column
+missing_percentage_cat = (data[cat_features].isnull().sum() / len(data[cat_features])) * 100
+print("Missing percentage Categorical columns\n", missing_percentage_cat)
+
+#Percentage of missing values per numeric column
+missing_percentage_num = (data[num_features].isnull().sum() / len(data[num_features])) * 100
+print("Missing percentage numeric columns\n", missing_percentage_num)
+
+
+
---
+++ Nata/Log Reg Model.py
@@ -0,0 +1,11 @@
+from Data_Preparation import data
+from Data_Preparation import label
+from Data_Preparation import features
+
+
+
+#SPLIT DATA
+
+#SCALE NUMERIC FEATURES
+
+#ONE HOT ENCODE CATEGORICAL FEATURES
